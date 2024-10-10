8481350a66dbba8fc66e7c9bc024573b5a9d1cc3
BoogieVugi <t.vugrinec73@gmail.com>
2024-10-10 20:23:27 +0200

    novi program sa do-while petljom

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int tajnibroj = 9;
+        int a;
+        do {
+            System.out.println("pogodi tajni broj");
+            a = scanner.nextInt();
 
-        System.out.println(" 1 označava zbrajanje");
-        System.out.println(" 2 označava oduzimanje");
-        System.out.println(" 3 označava množenje");
-        System.out.println(" 4 označava djeljenje");
+            if (a != tajnibroj) {
+                System.out.println("Pogrešan broj");
+            }
+        } while (a != tajnibroj);
 
-        System.out.print("Unesi broj od 1 do 4: ");
-        int a = scanner.nextInt();
-
-        if (a==1){
-            System.out.println("Zbrajanje je osnovna računska operacija, kojom se, dva ili više broja skupe zajedno.");
-        }
-        if (a == 2){
-            System.out.println("oduzimanje je matematička operacija kojom se određuje koliko se razlikuju dva broja");
-        }
-        if (a == 3){
-            System.out.println("Množenje prirodnih brojeva možemo shvatiti kao uzastopno zbrajanje jednakih pribrojnika.");
-        }
-        if (a == 4){
-            System.out.println("dijeljenje je osnovna matematička operacija kojom se određuje broj, polinom, varijabla");
-        }
-        }
+        System.out.println("Čestitam! Pogodio si tajni broj.");
+        scanner.close();
     }
+}
