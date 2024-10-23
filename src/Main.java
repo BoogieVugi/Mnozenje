6365ce4ca07672bbb165110533e830a1b6159944
BoogieVugi <t.vugrinec73@gmail.com>
2024-10-23 20:40:04 +0200

    zadatak sa predavanja

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-import com.sun.source.tree.SwitchExpressionTree;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[10];
-        
-        System.out.println("Unesite 10 brojeva:");
-        for (int i = 0; i < 10; i++) {
-            numbers[i] = scanner.nextInt();
+
+        System.out.println("unesite broj elemenata: ");
+        int brojElemenata = scanner.nextInt();
+
+        ArrayList<Integer> Lista = new ArrayList<>();
+
+        System.out.println("Unesite elemente liste: ");
+        for (int i = 0; i < brojElemenata; i++){
+            Lista.add(scanner.nextInt());
         }
+        // OVO DALJE SAM UZ POMOĆ INTERNETA RIJEŠIO
+
+        boolean duplikatPostoji = false;
+
+        for (int i = 0; i < Lista.size(); i++) {
+            for (int j = i + 1; j < Lista.size(); j++) {
+                if (Lista.get(i).equals(Lista.get(j))) {
+                    duplikatPostoji = true;
+                    break;
+                }
+            }
+            if (duplikatPostoji) {
+                break;
+            }
+        }
+
+        // Ispisi rezultat
+        if (duplikatPostoji) {
+            System.out.println("True - Duplikat postoji");
+        } else {
+            System.out.println("False - Nema duplikata");
+        }
+        scanner.close();
+
+    }
+}
