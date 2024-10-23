ad2e7c09f9c6bc3c38f7f7a50aa7b66ee461c9b6
BoogieVugi <t.vugrinec73@gmail.com>
2024-10-23 17:24:56 +0200

    drugi zadatak, zadaca 23.10.2024.

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,15 @@
 import com.sun.source.tree.SwitchExpressionTree;
 
-import java.lang.runtime.SwitchBootstraps;
-import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
 
-        public class Main {
-            public static void main(String[] args) {
-
-                List<Integer> numbers = new ArrayList<>();
-                Scanner scanner = new Scanner(System.in);
-
-
-                System.out.println("Unesite brojeve (za kraj unesite 'kraj'):");
-
-                while (scanner.hasNextInt()) {
-                    numbers.add(scanner.nextInt());
-                }
-
-
-                scanner.close();
-
-
-                if (numbers.isEmpty()) {
-                    System.out.println("Lista je prazna.");
-                    return;
-                }
-
-                int min = numbers.get(0);
-                int max = numbers.get(0);
-                int minIndex = 0;
-                int maxIndex = 0;
-
-                for (int i = 1; i < numbers.size(); i++) {
-                    if (numbers.get(i) < min) {
-                        min = numbers.get(i);
-                        minIndex = i;
-                    }
-                    if (numbers.get(i) > max) {
-                        max = numbers.get(i);
-                        maxIndex = i;
-                    }
-                }
-
-                numbers.set(minIndex, max);
-                numbers.set(maxIndex, min);
-                
-                System.out.println("Nova lista nakon zamjene: " + numbers);
-            }
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[10];
+        
+        System.out.println("Unesite 10 brojeva:");
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = scanner.nextInt();
         }
-
-
