3a7941d09922767612eff758c11fee1dbd22d14b
BoogieVugi <t.vugrinec73@gmail.com>
2024-10-30 16:42:11 +0100

    zadatak sa predavanja za zadacu 30/10/24

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        float []apoeni = [500,200,100,50,20,10,5,2,1,0.5,0.2,0.1];
+        double[] apoeni = {500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1};
 
-        System.out.println(" Upišite svotu novca koju je potrebno vratiti kupcu: ");
-        float kusur = scanner.nextFloat();
-        for (int i=0; int i < apoeni.length; i++);
-        float apoen = apoeni[i];
-        float kusur = apoeni[i];
-        float brojnovcanica = kusur / apoen;
+        System.out.print("Unesite iznos novca za povrat: ");
+        double iznosZaPovrat = scanner.nextDouble();
+
+
+        for (int i = 0; i < apoeni.length; i++) {
+            double apoen = apoeni[i];
+            int brojNovcanica = (int)(iznosZaPovrat / apoen);
+
+            if (brojNovcanica > 0) {
+                for (int j = 0; j < brojNovcanica; j++) {
+
+                    System.out.println("Povrat u apoenima:" + apoen + " ");
+                }
+                iznosZaPovrat -= brojNovcanica * apoen;
+                iznosZaPovrat = Math.round(iznosZaPovrat * 100.0) / 100.0;
+            }
+        }
     }
 }
-//Do tud sam došao, možda boljeda sam uzeo double umjesto floata za računicu.
+
